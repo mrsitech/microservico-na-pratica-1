4a8f2a392738c3401e72ff1afcc81ad0d0a74534
mrsitech <mrsitech@outlook.com>
2022-09-21 16:37:11 -0300

    Model constructor packege

---
+++ src/main/java/br/com/alurafood/pagamentos/model/Pagamento.java
@@ -0,0 +1,68 @@
+package br.com.alurafood.pagamentos.model;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.transaction.Status;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+
+@Entity//associa a classe ao DB
+@Table(name="pagamentos")//Pelo Entity procurar a table pagamentos
+@Getter//getters automatico sem codigo
+@Setter//Setters automatico sem codigo
+@AllArgsConstructor
+@NoArgsConstructor
+public class Pagamento {
+	
+	@Id //id unico na tabela
+	@GeneratedValue(strategy = GenerationType.IDENTITY)//Como os ids serão criados
+	private Long id;
+	
+	@NotNull
+	@Positive
+	private BigDecimal valor;
+	@NotBlank
+	@Size(max=100)
+	private String nome;
+	
+	@NotBlank
+	@Size(max=19)
+	private String numero;
+	
+
+	@NotBlank
+	@Size(max=7)
+	private String expiracao;
+	
+
+	@NotBlank
+	@Size(min=3, max=3)
+	private String codigo;
+	
+	@NotNull
+	@Enumerated(EnumType.STRING)
+	private Status status;
+	
+	@NotNull
+	private Long pedidoId;
+	
+	@NotNull
+	private Long formaDePagamentoId;
+
+}
